cfd711aee6697db1d6b82e3a6d8eb26122c61df9
rumkkee <accesareo@gmail.com>
2023-11-10 19:14:46 -0800

    Users.java table setup

---
+++ app/src/main/java/com/example/projectmanager_android/DB/AppDataBase.java
@@ -0,0 +1,8 @@
+package com.example.projectmanager_android.DB;
+
+public abstract class AppDataBase {
+    public static final String DATABASE_NAME = "ProjectManager.db";
+    public static final String USER_CREDENTIALS_TABLE = "users_table";
+
+
+}
---
+++ app/src/main/java/com/example/projectmanager_android/DB/Users.java
@@ -0,0 +1,59 @@
+package com.example.projectmanager_android.DB;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = AppDataBase.USER_CREDENTIALS_TABLE)
+public class Users {
+    @PrimaryKey(autoGenerate = true)
+    private int mUserId;
+    private String mUsername;
+    private String mPassword;
+    private boolean mIsAdmin;
+
+    public Users(String username, String password) {
+        mUsername = username;
+        mPassword = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User # " + mUserId + "\n" +
+                "Username: " + mUsername + "\n" +
+                "Password: " + mPassword + "\n" +
+                "IsAdmin: " + mIsAdmin + "\n" +
+                "=-=-=-=-=-=-";
+    }
+
+    public int getUserId() {
+        return mUserId;
+    }
+
+    public void setUserId(int userId) {
+        mUserId = userId;
+    }
+
+    public String getUsername() {
+        return mUsername;
+    }
+
+    public void setUsername(String username) {
+        mUsername = username;
+    }
+
+    public String getPassword() {
+        return mPassword;
+    }
+
+    public void setPassword(String password) {
+        mPassword = password;
+    }
+
+    public boolean getIsAdmin() {
+        return mIsAdmin;
+    }
+
+    public void setIsAdmin(boolean isAdmin) {
+        mIsAdmin = isAdmin;
+    }
+}
