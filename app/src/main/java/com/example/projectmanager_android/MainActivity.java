9b78eb6d1426e9adbc55a6924b532787a1daab55
rumkkee <accesareo@gmail.com>
2023-11-10 18:02:24 -0800

    MainActivity.java setup

--- app/src/main/java/com/example/projectmanager_android/MainActivity.java
+++ app/src/main/java/com/example/projectmanager_android/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.projectmanager_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.projectmanager_android.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mActivityMainBinding;
+    Button mLogInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mActivityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mActivityMainBinding.getRoot());
+
+        mLogInButton = mActivityMainBinding.mainActivityMainActivityLoginButton;
+
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Create the login activity,
+                //  then start its activity with an intent.
+            }
+        });
+
+    }
+
+    public static Intent getIntent(Context context){
+        Intent intent = new Intent(context, MainActivity.class);
+        return intent;
     }
 }
