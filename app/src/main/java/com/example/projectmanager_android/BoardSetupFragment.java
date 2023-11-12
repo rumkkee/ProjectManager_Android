a59d7589a874431a31a4b0803962226df5acce71
rumkkee <accesareo@gmail.com>
2023-11-12 10:57:53 -0800

    Set up BoardSetupFragment.java

---
+++ app/src/main/java/com/example/projectmanager_android/BoardSetupFragment.java
@@ -0,0 +1,76 @@
+package com.example.projectmanager_android;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BoardSetupFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BoardSetupFragment extends Fragment {
+
+    private EditText mBoardName;
+    private Button mBackButton;
+    private Button mSubmitButton;
+
+    public BoardSetupFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment.
+     * @return A new instance of fragment BoardSetupFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BoardSetupFragment newInstance() {
+        BoardSetupFragment fragment = new BoardSetupFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_board_setup, container, false);
+        // Inflate the layout for this fragment
+        mBoardName = view.findViewById(R.id.BoardName_input);
+        mBackButton = view.findViewById(R.id.BoardSetup_BackButton);
+        mSubmitButton = view.findViewById(R.id.BoardSetupComplete_button);
+
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeFragment();
+            }
+        });
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String boardNameInput = mBoardName.getText().toString();
+                removeFragment();
+            }
+        });
+
+        return view;
+    }
+
+    private void removeFragment(){
+        getParentFragmentManager().beginTransaction().remove(BoardSetupFragment.this).commit();
+    }
+
+}
