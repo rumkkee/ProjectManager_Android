6ed351dbb8fb4255c561039b38b0535dfade8ed8
rumkkee <accesareo@gmail.com>
2023-11-14 23:21:15 -0800

    Implemented UserViewModel.java This class is responsible for holding UI data in a manner that survives configuration changes.

---
+++ app/src/main/java/com/example/projectmanager_android/DB/UserViewModel.java
@@ -0,0 +1,27 @@
+package com.example.projectmanager_android.DB;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class UserViewModel extends AndroidViewModel {
+    private UserRepository mRepository;
+    private final LiveData<List<Users>> mAllUsers;
+
+    public UserViewModel(Application application){
+        super(application);
+        mRepository = new UserRepository(application);
+        mAllUsers = mRepository.getAllUsers();
+    }
+
+    LiveData<List<Users>> getAllUsers(){
+        return mAllUsers;
+    }
+
+    public void insert(Users user){
+        mRepository.insert(user);
+    }
+}
