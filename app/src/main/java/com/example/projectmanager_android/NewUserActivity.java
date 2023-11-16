cfcc8fa3a44698aa280a8af2b1a027f33f98aa66
rumkkee <accesareo@gmail.com>
2023-11-16 11:46:20 -0800

    Implemented NewUserActivity.java

---
+++ app/src/main/java/com/example/projectmanager_android/NewUserActivity.java
@@ -0,0 +1,39 @@
+package com.example.projectmanager_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NewUserActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY = "com.example.android.wordlistsql.REPLY";
+    private EditText mEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user);
+        mEditText = findViewById(R.id.edit_user);
+
+        final Button saveButton = findViewById(R.id.button_save);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent replyIntent = new Intent();
+                if(TextUtils.isEmpty(mEditText.getText())){
+                    setResult(RESULT_CANCELED, replyIntent);
+                }
+                else{
+                    String user = mEditText.getText().toString();
+                    replyIntent.putExtra(EXTRA_REPLY, user);
+                    setResult(RESULT_OK, replyIntent);
+                }
+                finish();
+            }
+        });
+    }
+}
