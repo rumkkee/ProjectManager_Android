36b54d6508ca6f770e35d247de20c1cbc54c78ec
rumkkee <accesareo@gmail.com>
2023-11-16 08:50:36 -0800

    Created the UserListAdapter.java class that extends ListAdapter<>

---
+++ app/src/main/java/com/example/projectmanager_android/DB/UserListAdapter.java
@@ -0,0 +1,40 @@
+package com.example.projectmanager_android.DB;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+
+public class UserListAdapter extends ListAdapter<Users, UserViewHolder> {
+
+    public UserListAdapter(@NonNull DiffUtil.ItemCallback<Users> diffCallback) {
+        super(diffCallback);
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return UserViewHolder.create(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        Users current = getItem(position);
+        holder.bind(current.getUsername());
+    }
+
+    static class UserDiff extends DiffUtil.ItemCallback<Users>{
+
+        @Override
+        public boolean areItemsTheSame(@NonNull Users oldItem, @NonNull Users newItem) {
+            return oldItem == newItem;
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Users oldItem, @NonNull Users newItem) {
+            return oldItem.getUsername().equals(newItem.getUsername());
+        }
+    }
+
+}
