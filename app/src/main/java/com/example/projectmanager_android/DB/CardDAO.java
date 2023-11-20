41192ab045e1ae6773851d1591287617a3f41b99
rumkkee <accesareo@gmail.com>
2023-11-20 12:52:48 -0800

    Implemented the DAO for the Card entity

---
+++ app/src/main/java/com/example/projectmanager_android/DB/CardDAO.java
@@ -0,0 +1,33 @@
+package com.example.projectmanager_android.DB;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface CardDAO {
+    @Insert
+    void insert(Card... cards);
+
+    @Update
+    void update(Card... cards);
+
+    @Delete
+    void delete(Card... cards);
+
+    @Query("SELECT * FROM " + AppDataBase.BOARDS_TABLE)
+    LiveData<List<Card>> getAllCards();
+
+    @Query("SELECT * FROM " + AppDataBase.BOARDS_TABLE +
+            " WHERE mUserId == :userId")
+    LiveData<List<Card>> getCardsByUserId(int userId);
+
+    @Query("SELECT * FROM " + AppDataBase.BOARDS_TABLE +
+            " WHERE mCardListId == :cardListId")
+    LiveData<List<Card>> getCardsByCardListId(int cardListId);
+}
