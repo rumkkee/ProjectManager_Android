cdf76458c306b04fd3231103c4d8a95c6fa55fb9
rumkkee <accesareo@gmail.com>
2023-11-20 13:16:42 -0800

    Implemented the ViewModel for the Card entity. This fixes #6

---
+++ app/src/main/java/com/example/projectmanager_android/DB/CardViewModel.java
@@ -0,0 +1,26 @@
+package com.example.projectmanager_android.DB;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class CardViewModel extends AndroidViewModel {
+    private CardRepository mRepository;
+    private final LiveData<List<Card>> mAllCards;
+
+    public CardViewModel(@NonNull Application application) {
+        super(application);
+        mRepository = new CardRepository(application);
+        mAllCards = mRepository.getAllCards();
+    }
+
+    public LiveData<List<Card>> getAllCards(){return mAllCards;}
+    public LiveData<List<Card>> getCardsByUserId(int userId){return mRepository.getCardsByUserId(userId);}
+    public LiveData<List<Card>> getCardsByCardListId(int cardListId){return mRepository.getCardsByCardListId(cardListId);}
+
+    public void insert(Card card){mRepository.insert(card);}
+}
