0becc06d3a09fd7318a4629db36a2405c4371b35
rumkkee <accesareo@gmail.com>
2023-11-24 18:09:13 -0800

    setup the abstract ItemAdderFragment.java class This class will be implemented by the CardListFragment and CardFragment classes

--- app/src/main/java/com/example/projectmanager_android/ItemAdderFragment.java
+++ app/src/main/java/com/example/projectmanager_android/ItemAdderFragment.java
@@ -7,13 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ItemAdderFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ItemAdderFragment extends Fragment {
+///**
+// * A simple {@link Fragment} subclass.
+// * Use the {@link ItemAdderFragment#newInstance} factory method to
+// * create an instance of this fragment.
+// */
+public abstract class ItemAdderFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,41 +26,80 @@ public class ItemAdderFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    protected EditText mItemNameEditText;
+    protected Button mCancelButton;
+    protected Button mAddButton;
+
+    protected static boolean isOpen;
+
     public ItemAdderFragment() {
         // Required empty public constructor
+        isOpen = true;
     }
 
     /**
      * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * this fragment.
      * @return A new instance of fragment ItemAdderFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static ItemAdderFragment newInstance(String param1, String param2) {
-        ItemAdderFragment fragment = new ItemAdderFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+//    public static ItemAdderFragment newInstance(/*String param1, String param2*/) {
+//        ItemAdderFragment fragment = new ItemAdderFragment();
+//        // Keeping this starter code for reference
+////        Bundle args = new Bundle();
+////        args.putString(ARG_PARAM1, param1);
+////        args.putString(ARG_PARAM2, param2);
+////        fragment.setArguments(args);
+//        return fragment;
+//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_item_adder, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item_adder, container, false);
+        mAddButton = view.findViewById(R.id.itemAdder_addButton);
+        mCancelButton = view.findViewById(R.id.itemAdder_cancelButton);
+        mItemNameEditText = view.findViewById(R.id.itemAdder_titleInput);
+
+        mCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeFragment();
+            }
+        });
+
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String itemNameInput = mItemNameEditText.getText().toString();
+                if(!itemNameInput.isEmpty()){
+
+                }
+            }
+        });
+
+        return view;
+    }
+
+
+    protected abstract void addItem(String itemName);
+
+    protected void removeFragment(){
+        isOpen = false;
+        getParentFragmentManager().beginTransaction().remove(ItemAdderFragment.this).commit();
+    }
+
+    public static boolean isOpen(){
+        return isOpen;
     }
 }
