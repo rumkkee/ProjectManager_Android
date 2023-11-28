f707183b36c8d7b27f836af7dd8b5075a1e6f419
rumkkee <accesareo@gmail.com>
2023-11-28 11:56:31 -0800

    Set up fragment_card_expanded.xml

--- app/src/main/java/com/example/projectmanager_android/CardExpandedFragment.java
+++ app/src/main/java/com/example/projectmanager_android/CardExpandedFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +25,8 @@ public class CardExpandedFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private TextView mCardTitle;
+    private TextView mCardDescription;
 
     public CardExpandedFragment() {
         // Required empty public constructor
@@ -60,6 +63,11 @@ public class CardExpandedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card_expanded, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_card_expanded, container, false);
+
+
+
+        return view;
     }
 }
