b69a62fbd98deb0f5430bb07b5a1ccf799c2f61e
rumkkee <accesareo@gmail.com>
2023-11-27 22:54:16 -0800

    Init CardAdderFragment.java

---
+++ app/src/main/java/com/example/projectmanager_android/CardAdderFragment.java
@@ -0,0 +1,8 @@
+package com.example.projectmanager_android;
+
+import com.example.projectmanager_android.DB.CardViewModel;
+
+public class CardAdderFragment extends ItemAdderFragment{
+    CardViewModel mCardViewModel;
+
+}
