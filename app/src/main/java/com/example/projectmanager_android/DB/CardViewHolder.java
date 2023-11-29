edc92902a7da1cc0c694c4ea10d569728c36488f
rumkkee <accesareo@gmail.com>
2023-11-28 16:30:52 -0800

    Clicking on a Card opens the CardExpandedFragment. The CardViewHolder uses an interface to instruct the BoardActivity to open the CardExpandedFragment.

--- app/src/main/java/com/example/projectmanager_android/DB/CardViewHolder.java
+++ app/src/main/java/com/example/projectmanager_android/DB/CardViewHolder.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.projectmanager_android.CardDisplayer;
 import com.example.projectmanager_android.CardExpandedFragment;
 import com.example.projectmanager_android.R;
 
@@ -15,9 +16,9 @@ public class CardViewHolder extends RecyclerView.ViewHolder {
     private final TextView cardTitleTextView;
     private int cardId;
     private String cardName;
+    private Card mCard;
 
-    // TODO: Uncomment once I've created the CardDisplayer interface
-    //private CardDisplayer mCardDisplayer;
+    private CardDisplayer mCardDisplayer;
 
     public CardViewHolder(@NonNull View itemView) {
         super(itemView);
@@ -26,36 +27,24 @@ public class CardViewHolder extends RecyclerView.ViewHolder {
         cardTitleTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: Call the method displayCardExpandedFragment(cardId) from this class' field of CardDisplayer interface.
-                //CardExpandedFragment cardExpandedFragment = CardExpandedFragment.newInstance("title", "desc");
-
-//                itemView.getRootView().getContext().getSupportFragmentManager().beginTransaction()
-//                        .add(android.R.id.content, cardExpandedFragment)
-//                        .addToBackStack(null)
-//                        .commit();
+                mCardDisplayer.displayCardExpandedFragment(cardId);
             }
         });
     }
 
-    // TODO: Add a param of Interface CardDisplayer
-    public void bind(int cardId, String cardName){
+
+    public void bind(int cardId, String cardName, CardDisplayer cardDisplayer){
         cardTitleTextView.setText(cardName);
         this.cardId = cardId;
         this.cardName = cardName;
+        this.mCardDisplayer = cardDisplayer;
     }
 
     public static CardViewHolder create(ViewGroup parent){
-        // TODO: Make a recyclerView specific for cards.
-        //  Update the layout to a recyclerView.
-        //  (For now, the recyclerView for boards will suffice.)
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.recyclerview_card, parent, false);
 
         return new CardViewHolder(view);
-    }
-
-    private void showCardExpandedFragment(){
-
     }
 
 }
