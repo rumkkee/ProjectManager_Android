82ac9c45f1e61c8145fc78cdaca127210fc76a19
rumkkee <accesareo@gmail.com>
2023-11-28 16:02:10 -0800

    Wrote todos in the CardViewHolder.java for once the CardDisplayer interface is implemented

--- app/src/main/java/com/example/projectmanager_android/DB/CardViewHolder.java
+++ app/src/main/java/com/example/projectmanager_android/DB/CardViewHolder.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.projectmanager_android.CardExpandedFragment;
 import com.example.projectmanager_android.R;
 
 public class CardViewHolder extends RecyclerView.ViewHolder {
@@ -15,13 +16,28 @@ public class CardViewHolder extends RecyclerView.ViewHolder {
     private int cardId;
     private String cardName;
 
+    // TODO: Uncomment once I've created the CardDisplayer interface
+    //private CardDisplayer mCardDisplayer;
+
     public CardViewHolder(@NonNull View itemView) {
         super(itemView);
          cardTitleTextView = itemView.findViewById(R.id.recyclerView_Card);
 
+        cardTitleTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Call the method displayCardExpandedFragment(cardId) from this class' field of CardDisplayer interface.
+                //CardExpandedFragment cardExpandedFragment = CardExpandedFragment.newInstance("title", "desc");
 
+//                itemView.getRootView().getContext().getSupportFragmentManager().beginTransaction()
+//                        .add(android.R.id.content, cardExpandedFragment)
+//                        .addToBackStack(null)
+//                        .commit();
+            }
+        });
     }
 
+    // TODO: Add a param of Interface CardDisplayer
     public void bind(int cardId, String cardName){
         cardTitleTextView.setText(cardName);
         this.cardId = cardId;
@@ -36,6 +52,10 @@ public class CardViewHolder extends RecyclerView.ViewHolder {
                 .inflate(R.layout.recyclerview_card, parent, false);
 
         return new CardViewHolder(view);
+    }
+
+    private void showCardExpandedFragment(){
+
     }
 
 }
